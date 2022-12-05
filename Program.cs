9070c082a4ab07c683ebeef9b717d11977bf4d9a
YogitaDevi <117684801+YogitaDevi@users.noreply.github.com>
2022-12-05 23:09:24 +0530

    UC2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC2_Snakes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int genRand = r.Next(1, 6);
+            Console.WriteLine("Random Number = " + genRand);
+        }
+    }
+}
