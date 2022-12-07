d622f93614e9d300fc5e430159e7ee9f1ba3b259
YogitaDevi <yogita.gry@gmail.com>
2022-12-07 16:20:14 +0530

    SnakeAndLadder

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SnakeAndLadder;
+
+Console.WriteLine("Welcome to Snake And Ladder Program");
+
+Console.WriteLine("Calling Player Final Report");
+FinalReport finalReport = new FinalReport();
+finalReport.Report();
+
+Console.WriteLine("Calling Player to win the Game");
+WinTheGame theGame = new WinTheGame();
+theGame.win();
+
+Console.WriteLine("Calling Player Exact Winning Position");
+ExactWinningPosition ExactWinning = new ExactWinningPosition();
+ExactWinning.Exact();
+
+Console.WriteLine("Calling Player Reaches Winning Position");
+ReachesWinningPosition winningPosition = new ReachesWinningPosition();
+winningPosition.Reaches();
+
+Console.WriteLine("Calling ChecksForOption");
+ChecksForOption checks = new ChecksForOption();
+checks.checkoption();
+
+Console.WriteLine("Calling RollstheDice");
+RollsTheDice rollstheDice = new RollsTheDice();
+rollstheDice.Dice();
+
+Console.WriteLine("Calling Game Start Position");
+StartPosition startPosition = new StartPosition();
+startPosition.GameStart();
+
+Console.ReadKey();
+Console.WriteLine();
+
+
